c5a78fa973146caf7b7522fcde4dac6da36f47bc
Tezel Zenginoğlu <88385119+Zenginoglu@users.noreply.github.com>
2024-09-28 01:16:09 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import "../src/style.css";
+const content = [
+  [
+    "React is extremely popular",
+    "It makes building complex, interactive UIs a breeze",
+    "It's powerful & flexible",
+    "It has a very active and versatile ecosystem"
+  ],
+  [
+    "Components, JSX & Props",
+    "State",
+    "Hooks (e.g., useEffect())",
+    "Dynamic rendering"
+  ],
+  [
+    "Official web page (react.dev)",
+    "Next.js (Fullstack framework)",
+    "React Native (build native mobile apps with React)"
+  ],
+  [
+    "Vanilla JavaScript requires imperative programming",
+    "Imperative Programming: You define all the steps needed to achieve a result",
+    "React on the other hand embraces declarative programming",
+    "With React, you define the goal and React figures out how to get there"
+  ]
+];
+
+function App() {
+const [activeContent, setActiveContent] = useState(0);
+  return (
+    <div className="mainContainer">
+      <header>
+        <img src="../src/assets/react.svg" alt="React logo" />
+        <div>
+          <h1>React.js</h1>
+          <p>i.e., using the React library for rendering the UI</p>
+        </div>
+      </header>
+
+      <div id="tabs">
+        <menu>
+          <button
+            className={activeContent === 0 ? "active" : ""}
+            onClick={() => setActiveContent(0)}
+          >
+            Why React?
+          </button>
+          <button
+            className={activeContent === 1 ? "active" : ""}
+            onClick={() => setActiveContent(1)}
+          >
+            Core Features
+          </button>
+          <button
+            className={activeContent === 2 ? "active" : ""}
+            onClick={() => setActiveContent(2)}
+          >
+            Related Resources
+          </button>
+          <button
+            className={activeContent === 3 ? "active" : ""}
+            onClick={() => setActiveContent(3)}
+          >
+            React vs Javascript
+          </button>
+        </menu>
+        <div id="tab-content">
+          <ul>
+            {content[activeContent].map((item) => (
+              <li key={item}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App
